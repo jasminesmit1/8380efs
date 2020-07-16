ee019235a5fabec2d28cd61e48adbf1614f74396
Jasmine Smith <jasminesmit1@unomaha.edu>
2020-07-16 01:56:49 -0500

    trying to fix admin on heroku

--- efs/urls.py
+++ efs/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    path('users/', include('django.contrib.auth.urls')),
 ]
