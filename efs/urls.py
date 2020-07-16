c98ddf7b89ab71477a80cb7799e6fd0a4679fcc8
Jasmine Smith <jasminesmit1@unomaha.edu>
2020-07-16 01:26:01 -0500

    staticfiles

--- efs/urls.py
+++ efs/urls.py
@@ -32,9 +32,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
-path('', include('django.contrib.auth.urls')),
 
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
